4ddc2b7f2bc5aa8bbfa750fe57bb45afb53805bc
wernjieyew <wernjieyew@gmail.com>
2024-04-30 07:13:37 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$currency = '₹';
+$db_username = 'root';
+$db_password = 'database';
+$db_name = 'bolt';
+$db_host = 'test2.cve8wc6o4b25.us-east-1.rds.amazonaws.com';
+$mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
+?>
